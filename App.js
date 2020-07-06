f1ff112e7e06e4791e3ec1bb540024468dc358a6
Shyam Tiwari <shyam02062001@gmail.com>
2020-07-06 19:38:48 +0530

    Footer workin properly

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View, Image, Button } from "react-native"
+import * as Google from 'expo-google-app-auth'
+import Home from './Pages/Home'
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      signedIn: false,
+      name: "",
+      photoUrl: ""
+    }
+  }
+  signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "265455786512-dthodismibrhetj7c35ot274083a8hga.apps.googleusercontent.com",
+        //iosClientId: YOUR_CLIENT_ID_HERE,  <-- if you use iOS
+        scopes: ["profile", "email"]
+      })
 
-export default function App() {
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.signedIn ? (
+          <LoggedInPage name={this.state.name} photoUrl={this.state.photoUrl} />
+        ) : (
+          <LoginPage signIn={this.signIn} />
+        )}
+      </View>
+    )
+  }
+}
+
+const LoginPage = props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" onPress={() => props.signIn()} />
     </View>
-  );
+  )
+}
+
+const LoggedInPage = props => {
+  return (
+        <Home/>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  header: {
+    fontSize: 25
+  },
+  footer:{
+    flex:1,
+    width:450
+  }
+})
+
+
---
+++ Pages/Profile.js
@@ -0,0 +1,100 @@
+
+import React, { Component } from 'react'
+import {View,StyleSheet,Dimensions,Text,TouchableOpacity,Image} from "react-native"
+import Home from './Home'
+import Add from './Add'
+import Search from './Search' 
+import Create from './Create'
+export class Profile extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             number:1,
+             number1:1,
+             number2:1,
+             number3:1,
+        }
+    }
+    clickHandler=()=>{
+        this.setState({
+            number:2
+        })
+    }
+
+    clickHandler1=()=>{
+        this.setState({
+            number1:2
+        })
+    }
+    clickHandler2=()=>{
+        this.setState({
+            number2:2
+        })
+    }
+    clickHandler3=()=>{
+        this.setState({
+            number3:2
+        })
+    }
+    render() {
+        if(this.state.number===2){
+            return(<Home/>)
+        }
+        if(this.state.number1===2){
+            return(<Add/>)
+        }
+        if(this.state.number2===2){
+            return(<Search/>)
+        }
+        if(this.state.number3===2){
+            return(<Create/>)
+        }
+
+    return (
+         <View style={styles.footer}>
+        <TouchableOpacity onPress={this.clickHandler}>
+              
+          <Image source={require('../assets/home.png')} style={{height:20,width:20}}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={this.clickHandler1}
+          >
+          <Image source={require('../assets/add.png')} style={{height:20,width:20}}/>
+        </TouchableOpacity>
+        <TouchableOpacity  onPress={this.clickHandler2}
+          >
+          <Image source={require('../assets/search.png')} style={{height:20,width:20}}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.clickHandler3}
+          >
+          <Image source={require('../assets/create.png')} style={{height:20,width:20}}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          ><Text>Profile</Text>
+          <Image source={require('../assets/profile.png')} style={{height:20,width:20}}/>
+        </TouchableOpacity>
+      </View>
+    )
+}
+}
+
+const styles = StyleSheet.create({
+footer: {
+  flex: 1,
+  flexDirection:'row',
+  paddingHorizontal: 10,
+  position:'absolute',
+  bottom:3,
+  height:'8%',
+  alignItems:'center',
+  backgroundColor: "#DDDDDD",
+  justifyContent:'space-around',
+  width: Dimensions.get('window').width
+}
+});
+
+
+export default Profile
