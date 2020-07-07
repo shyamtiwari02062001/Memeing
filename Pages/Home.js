14cf9b2e0d91ebebcd2a082b3d88fd1d37259248
Shyam Tiwari <shyam02062001@gmail.com>
2020-07-07 09:03:07 +0530

    Home page flatlist added successfully

--- Pages/Home.js
+++ Pages/Home.js
@@ -7,12 +7,73 @@ import {
   TouchableOpacity,
   Image,
   SafeAreaView,
+  FlatList,
+  ScrollView,
 } from "react-native";
+import { Card } from 'react-native-elements';
 import Add from "./Add";
 import Search from "./Search";
 import Create from "./Create";
 import Profile from "./Profile";
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id:'2',
+    title:"ShyamJi"
+  },
+  {
+    id:'3',
+    title:"ShyamTiwari"
+  },
+  {
+    id:'4',
+    title:"Shyam"
+  },
+  {
+    id:'6',
+    title:"Shyam"
+  },
+  {
+    id:'7',
+    title:"Shyam"
+  },{
+    id:'8',
+    title:"Shyam"
+  },{
+    id:'9',
+    title:"Shyam"
+  },{
+    id:'10',
+    title:"Shyam"
+  },{
+    id:'11',
+    title:"Shyam"
+  },{
+    id:'12',
+    title:"Shyam"
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Card style={styles.card}>
+      <Text>{title}</Text></Card>
+  </View>
+);
 export class Home extends Component {
+  
   constructor(props) {
     super(props);
 
@@ -44,7 +105,15 @@ export class Home extends Component {
       number3: 2,
     });
   };
+
+  
+  
+  
+  
   render() {
+    const renderItem = ({ item }) => (
+      <Item title={item.title} />
+    );
     if (this.state.number === 2) {
       return <Add />;
     }
@@ -58,12 +127,19 @@ export class Home extends Component {
     if (this.state.number3 === 2) {
       return <Profile />;
     }
-
+    
     return (
       <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
-        <View style={styles.header}>
+
+<View style={styles.header}>
           <Text style={{ fontSize: 37 }}>Memeing</Text>
         </View>
+        <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        style={styles.flatlist}
+      />
         <View style={styles.footer}>
           <TouchableOpacity>
             <Image
@@ -124,6 +200,14 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     width: Dimensions.get("window").width,
   },
+  card:{
+  },
+  flatlist: {
+    marginTop:'15%',
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+ 
 });
 
 export default Home;
